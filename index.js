b300cd82a1761c4aed4a44de2ebaa51e414ef905
Adam Towers <hello@adamtowers.design>
2018-01-15 19:07:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,12 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase) {
+    console.log('I can\'t hear you')
+  }
+  else if (string.toUpperCase) {
+    console.log('YES INDEED!')
+  }
+}
